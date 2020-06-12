ef6bec29dab2d0adeec014b18d8ca10a26cb9548
Luckyaremu <aremu.baba70@gmail.com>
2020-06-12 11:22:00 +0100

    fixed the sign up, cover pages and profile pages, added linters and fixed errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root 'opinions#index'
-  resources :opinions, only: [:index, :create] do 
+  resources :opinions, only: %i[index create] do
     resources :comments, only: [:create]
   end
 
-  devise_for :users, except: [:show, :index]
-  resources :users, only: [:show, :index]
+  devise_for :users, except: %i[show index]
+  resources :users, only: %i[show index]
   get 'follow' => 'friendships#follow'
   post 'follow' => 'friendships#follow'
   delete 'unfollow' => 'friendships#unfollow'
--- db/migrate/20200604143640_add_column_to_users.rb
+++ db/migrate/20200604143640_add_column_to_users.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddColumnToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :first_names, :string
